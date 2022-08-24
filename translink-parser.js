acf5bfbb3470462af1a290c100b48f31122a5ca5
William Sawyer <wmsawyer2609@gmail.com>
2022-08-24 20:54:25 +1000

    Removed shapes from result

--- translink-parser.js
+++ translink-parser.js
@@ -248,7 +248,7 @@ async function getAgain(attempts = 0, previous = null) {
 }
 
 let staticData = await getStaticData();
-({ routes, shapes, stops, stopTimes, trips, calendarDates, calendar } = staticData);
+({ routes, stops, stopTimes, trips, calendarDates, calendar } = staticData);
 
 let apiData = await retrieveApiData();
 ([alerts, tripUpdates, vehiclePositions] = apiData);
